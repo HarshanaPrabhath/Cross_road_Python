5742ab11a390bab0a5c2e20e0d0d17252e543b86
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2024-02-14 11:11:00 +0530

    Completed Project and Add scoreboard class

--- main.py
+++ main.py
@@ -8,7 +8,33 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+car_manager = CarManager()
+player = Player()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_forward, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # detect collision turtle with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            score.game_over()
+            game_is_on = False
+
+
+    # detect turtle pass finish line
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        score.level_increased()
+
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,30 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.level = 1
+        self.hideturtle()
+        self.goto(-250, 220)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level : {self.level}", align="left", font=FONT)
+
+    def level_increased(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 50)
+        self.write(f"Level : {self.level}", align="center", font=("Courier", 10, "normal"))
+        self.goto(0, 0)
+        self.write("GAME_OVER", align="center", font=FONT)
+
